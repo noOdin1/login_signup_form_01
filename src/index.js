12d1129e21b292fbf35fd2d2b7b84fe4ae0a5d93
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-11-28 05:52:22 +0800

    addEventListener for login and signup link, to switch between the forms

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 /* index.js - login_signup_form_01 */
 import "./style.css";
+
+let loginLnk = document.getElementById("loginLink");
+let signupLnk = document.getElementById("signupLink");
+
+function changeForm(event) {
+  let signupCard = document.getElementById("signupCard");
+  signupCard.classList.toggle("hide");
+  let loginCard = document.getElementById("loginCard");
+  loginCard.classList.toggle("hide");
+}
+
+loginLnk.addEventListener("click", changeForm);
+signupLnk.addEventListener("click", changeForm);
