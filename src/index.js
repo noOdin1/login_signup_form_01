9b8d18baa68e7a7c8a5180e725ec306a0a56a460
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-12-09 11:03:16 +0800

    Assign handles for buttons. Function to simplify console logs.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,34 @@ import "./style.css";
 let loginLnk = document.getElementById("loginLink");
 let signupLnk = document.getElementById("signupLink");
 
+let signupSubmitBtn = document.getElementById("signupBtn");
+let loginSubmitBtn = document.getElementById("loginBtn");
+
+function con(obj) {
+  if (obj === null || obj === undefined) {
+    console.log("obj is null or undefined");
+    return;
+  }
+
+  /* src: https://stackoverflow.com/questions/4059147/check-if-a-variable-is-a-string-in-javascript */
+  if (typeof obj === "string" || obj instanceof String) {
+    console.log(obj);
+    return;
+  }
+
+  if (Array.isArray(obj)) {
+    obj.forEach((item) => {
+      console.log(item);
+    });
+  }
+
+  // Source - https://stackoverflow.com/questions/8511281/check-if-a-value-is-an-object-in-javascript
+  if (typeof obj === "object" && !Array.isArray(obj) && x !== null) {
+    console.dir(obj);
+    return;
+  }
+}
+
 function changeForm(event) {
   let signupCard = document.getElementById("signupCard");
   signupCard.classList.toggle("hide");
